962bf8eafe93f37c82b92925bd25eef74e492d46
GabrielFlores8227 <jgdaflores184186@hotmail.com>
2024-01-01 13:58:14 -0300

    update

--- src/app/common/components.page/about-page/about-page.component.ts
+++ src/app/common/components.page/about-page/about-page.component.ts
@@ -7,26 +7,26 @@ import { GrupoDeEstudoComponent } from './components.internal/grupo-de-estudo/gr
 import { HistoricoComponent } from './components.internal/historico/historico.component';
 import { ObjetivosEComposicaoComponent } from './components.internal/objetivos-e-composicao/objetivos-e-composicao.component';
 import { DescritivoSobreLogoComponent } from './components.internal/descritivo-sobre-logo/descritivo-sobre-logo.component';
-import { GentiComponent } from "./components.internal/genti/genti.component";
-import { EnsinoExtensaoPesquisaComponent } from "./components.internal/ensino-extensao-pesquisa/ensino-extensao-pesquisa.component";
-import { FormacaoDaComissaoComponent } from "./components.internal/formacao-da-comissao/formacao-da-comissao.component";
+import { GentiComponent } from './components.internal/genti/genti.component';
+import { EnsinoExtensaoPesquisaComponent } from './components.internal/ensino-extensao-pesquisa/ensino-extensao-pesquisa.component';
+import { FormacaoDaComissaoComponent } from './components.internal/formacao-da-comissao/formacao-da-comissao.component';
 
 @Component({
-    selector: 'app-about-page',
-    standalone: true,
-    templateUrl: './about-page.component.html',
-    styleUrl: './about-page.component.scss',
-    imports: [
-        RegularHeaderComponent,
-        RegularFooterComponent,
-        GrupoDeEstudoComponent,
-        HistoricoComponent,
-        ObjetivosEComposicaoComponent,
-        DescritivoSobreLogoComponent,
-        GentiComponent,
-        EnsinoExtensaoPesquisaComponent,
-        FormacaoDaComissaoComponent
-    ]
+  selector: 'app-about-page',
+  standalone: true,
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.scss',
+  imports: [
+    RegularHeaderComponent,
+    RegularFooterComponent,
+    GrupoDeEstudoComponent,
+    HistoricoComponent,
+    ObjetivosEComposicaoComponent,
+    DescritivoSobreLogoComponent,
+    GentiComponent,
+    EnsinoExtensaoPesquisaComponent,
+    FormacaoDaComissaoComponent,
+  ],
 })
 export class AboutPageComponent {
   public readonly homePageHeaderService = new HomePageHeaderService();
