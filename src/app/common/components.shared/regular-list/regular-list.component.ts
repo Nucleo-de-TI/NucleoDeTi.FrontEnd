3b14c9496bc19aa45d7115c763f1252d44f75079
GabrielFlores8227 <jgdaflores184186@hotmail.com>
2024-01-02 05:20:14 -0300

    flex-box-directive working

--- src/app/common/components.shared/regular-list/regular-list.component.ts
+++ src/app/common/components.shared/regular-list/regular-list.component.ts
@@ -2,11 +2,12 @@ import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { IRegularFlexBoxMedia } from '../../models.shared/regular-flex-box/regular-flex-box';
 import { CommonModule } from '@angular/common';
 import { IRegularList } from '../../models.shared/regular-list/regular-list';
+import { RegularFlexBoxDirective } from '../../directives/regular-flex-box/regular-flex-box.directive';
 
 @Component({
   selector: 'app-regular-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RegularFlexBoxDirective],
   templateUrl: './regular-list.component.html',
   styleUrl: './regular-list.component.scss',
 })
@@ -23,46 +24,5 @@ export class RegularListComponent implements OnInit {
     ) {
       this.listClassController['--padding-left'] = true;
     }
-
-    if (!this.model.media) {
-      return;
-    }
-
-    this.model.media.unshift(
-      JSON.parse(JSON.stringify({ 'min-width': 0, style: this.model.style }))
-    );
-
-    this.setStyle();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(): void {
-    this.setStyle();
-  }
-
-  private setStyle() {
-    if (!this.model.media) {
-      return;
-    }
-
-    const currentWidth = /Mobi|Android/i.test(navigator.userAgent)
-      ? document.documentElement.clientWidth
-      : window.innerWidth;
-    let pickedQuery: IRegularFlexBoxMedia = JSON.parse(
-      JSON.stringify(this.model.media[0])
-    );
-
-    this.model.media!.forEach((query) => {
-      if (query['min-width'] <= currentWidth) {
-        if (
-          pickedQuery === undefined ||
-          query['min-width'] > pickedQuery['min-width']
-        ) {
-          pickedQuery = query;
-        }
-      }
-    });
-
-    this.model.style = JSON.parse(JSON.stringify(pickedQuery.style));
   }
 }
---
+++ src/app/common/directives/regular-flex-box/regular-flex-box.directive.spec.ts
@@ -0,0 +1,8 @@
+import { RegularFlexBoxDirective } from './regular-flex-box.directive';
+
+describe('RegularFlexBoxDirective', () => {
+  it('should create an instance', () => {
+    const directive = new RegularFlexBoxDirective();
+    expect(directive).toBeTruthy();
+  });
+});
