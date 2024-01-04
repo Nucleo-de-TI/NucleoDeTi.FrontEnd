205428e6020852fc0b436650a2dc7bd4b2a03f20
GabrielFlores8227 <jgdaflores184186@hotmail.com>
2024-01-03 21:19:30 -0300

    update

--- src/app/common/components.shared/animated-text-link/animated-text-link.component.ts
+++ src/app/common/components.shared/animated-text-link/animated-text-link.component.ts
@@ -2,13 +2,14 @@ import { Component, Input } from '@angular/core';
 import { RegularTextLinkComponent } from '../regular-text-link/regular-text-link.component';
 import { IAnimatedLink } from '../../models/animated-link/animated-link';
 import { CommonModule } from '@angular/common';
+import { RegularTextComponent } from "../regular-text/regular-text.component";
 
 @Component({
-  selector: 'app-animated-text-link',
-  standalone: true,
-  templateUrl: './animated-text-link.component.html',
-  styleUrl: './animated-text-link.component.scss',
-  imports: [CommonModule, RegularTextLinkComponent],
+    selector: 'app-animated-text-link',
+    standalone: true,
+    templateUrl: './animated-text-link.component.html',
+    styleUrl: './animated-text-link.component.scss',
+    imports: [CommonModule, RegularTextLinkComponent, RegularTextComponent]
 })
 export class AnimatedTextLinkComponent {
   @Input() href!: string;
--- src/app/common/directives/regular-flex-box/regular-flex-box.directive.ts
+++ src/app/common/directives/regular-flex-box/regular-flex-box.directive.ts
@@ -1,43 +1,46 @@
 import { Directive, HostListener, Input, OnInit } from '@angular/core';
 import {
-  IRegularFlexBox, IRegularFlexBoxMedia } from '../../models/regular-flex-box/regular-flex-box';
+  IRegularFlexBox,
+  IRegularFlexBoxMedia,
+} from '../../models/regular-flex-box/regular-flex-box';
 
 @Directive({
   selector: '[appRegularFlexBox]',
-  standalone: true
+  standalone: true,
 })
 export class RegularFlexBoxDirective implements OnInit {
-    @Input() model!: IRegularFlexBox;
-  
-    ngOnInit(): void {
+  @Input() model!: IRegularFlexBox;
+
+  ngOnInit(): void {
+    if (!this.model.media) {
+      return;
+    }
+
+    this.model.media.unshift(
+      JSON.parse(JSON.stringify({ 'min-width': 0, style: this.model.style }))
+    );
+
+    this.setStyle();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    this.setStyle();
+  }
+
+  private setStyle() {
+    try {
       if (!this.model.media) {
         return;
       }
-  
-      this.model.media.unshift(
-        JSON.parse(JSON.stringify({ 'min-width': 0, style: this.model.style })),
-      );
-  
-      this.setStyle();
-    }
-  
-    @HostListener('window:resize', ['$event'])
-    onResize(): void {
-      this.setStyle();
-    }
-  
-    private setStyle() {
-      if (!this.model.media) {
-        return;
-      }
-  
+
       const currentWidth = /Mobi|Android/i.test(navigator.userAgent)
         ? document.documentElement.clientWidth
         : window.innerWidth;
       let pickedQuery: IRegularFlexBoxMedia = JSON.parse(
-        JSON.stringify(this.model.media[0]),
+        JSON.stringify(this.model.media[0])
       );
-  
+
       this.model.media!.forEach((query) => {
         if (query['min-width'] <= currentWidth) {
           if (
@@ -48,8 +51,12 @@ export class RegularFlexBoxDirective implements OnInit {
           }
         }
       });
-  
+
       this.model.style = JSON.parse(JSON.stringify(pickedQuery.style));
+    } catch (err: any) {
+      if (err.name !== 'ReferenceError') {
+        throw err;
+      }
     }
   }
-  
+}
