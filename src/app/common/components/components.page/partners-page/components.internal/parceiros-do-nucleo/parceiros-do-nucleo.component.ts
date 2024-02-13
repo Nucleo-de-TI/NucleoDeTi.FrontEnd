17835ed4a6ee964efd75f3980dc2be5c98523b6d
GabrielFlores8227 <jgdaflores184186@hotmail.com>
2024-02-13 18:13:47 -0300

    update

--- src/app/common/components/components.page/partners-page/components.internal/parceiros-do-nucleo/parceiros-do-nucleo.component.ts
+++ src/app/common/components/components.page/partners-page/components.internal/parceiros-do-nucleo/parceiros-do-nucleo.component.ts
@@ -4,13 +4,15 @@ import { RegularSectionBoxTitleComponent } from "../../../../components.shared/r
 import { RegularFlexBoxComponent } from "../../../../components.shared/regular-flex-box/regular-flex-box.component";
 import { SmallVerticalPaddingComponent } from "../../../../components.shared/small-vertical-padding/small-vertical-padding.component";
 import { BorderTextLinkComponent } from "../../../../components.shared/border-text-link/border-text-link.component";
+import { RegularTextComponent } from "../../../../components.shared/regular-text/regular-text.component";
+import { RegularBrComponent } from "../../../../components.shared/regular-br/regular-br.component";
 
 @Component({
     selector: 'app-parceiros-do-nucleo',
     standalone: true,
     templateUrl: './parceiros-do-nucleo.component.html',
     styleUrl: './parceiros-do-nucleo.component.scss',
-    imports: [RegularSectionBoxComponent, RegularSectionBoxTitleComponent, RegularFlexBoxComponent, SmallVerticalPaddingComponent, BorderTextLinkComponent]
+    imports: [RegularSectionBoxComponent, RegularSectionBoxTitleComponent, RegularFlexBoxComponent, SmallVerticalPaddingComponent, BorderTextLinkComponent, RegularTextComponent, RegularBrComponent]
 })
 export class ParceirosDoNucleoComponent {
 
